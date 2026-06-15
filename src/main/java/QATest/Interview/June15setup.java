6112e91c86579af71e4bb6ae957659da01fc0eec
Kavya Neelapala <kavyaneelapala@gmail.com>
2026-06-15 20:15:21 +0530

    Initial Interview

---
+++ src/main/java/QATest/Interview/June15setup.java
@@ -0,0 +1,15 @@
+package QATest.Interview;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class June15setup {
+	
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.get("abc");
+	
+	
+	}
+
+}
